1b1721dfb27837483812091b73e1b44315886e8e
FloweringVivian <flowering_season@163.com>
2017-03-29 16:37:33 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+//引入gulp
+var gulp = require("gulp");
+//引入组件
+var jshint = require("gulp-jshint");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+
